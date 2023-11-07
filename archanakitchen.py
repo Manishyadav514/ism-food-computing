361e0bf2b799b57612f0686d972447e1f1fc357a
Khushbu yadav <khushbuyad123@gmail.com>
2023-11-07 11:59:22 +0530

    working archana scraping

--- archanakitchen.py
+++ archanakitchen.py
@@ -10,14 +10,14 @@ import csv
 
 # Set the path to your webdriver executable (e.g., Chrome or Edge)
 # driver = webdriver.Edge()
-url = "https://www.archanaskitchen.com/"
-
+url="https://www.archanaskitchen.com/index.php?option=com_akrecipes&lang=en&limit=24&limitstart=312&view=frontpage"
 # Set the path to your webdriver executable (e.g., Chrome or Edge)
 driver = webdriver.Edge()
-data = []
+# data = []
 
 
 class Flipkart:
+    data = []
     def __init__(self):
         # Load the webpage
         driver.get(url)  # Replace 'your_webpage_url' with the actual URL
@@ -26,6 +26,7 @@ class Flipkart:
         # wait = WebDriverWait(driver, 10)
 
     def loadDataOnEachPage(self):
+        self.data = []
         # Find all elements with the specified structure
         elements = driver.find_elements(
             By.XPATH, "//div[@class='ak_frontpage_item mb-2 col-md-3']"
@@ -68,36 +69,9 @@ class Flipkart:
                 recipe_url = "no-url"
             # Print the extracted information
             # Append the data to the list
-            data.append([food_title, author_name, image_url, recipe_url])
+            self.data.append([food_title, author_name, image_url, recipe_url])
 
-    # def nextButtonClick():
-    #     # Find all "a" elements within "li" elements with the class 'page-item'
-    #     buttons = driver.find_elements(By.XPATH, "//li[contains(@class, 'page-item')]//a")
-    #     next_button = next(button for button in buttons if button.text == "Next")
-    #     next_button.click()
-    def saveDataInCSV(self):
-        # Save the data to a CSV file
-        csv_filename = "food_data.csv"
-
-        with open(csv_filename, "w", newline="") as csvfile:
-            csv_writer = csv.writer(csvfile)
-
-            # Write the header row
-            csv_writer.writerow(
-                ["Food Title", "Author Name", "Image URL", "Recipe URL"]
-            )
-
-            # Write the data rows
-            csv_writer.writerows(data)
-
-        print(f"Data saved to {csv_filename}")
-
-
-if __name__ == "__main__":
-    Flipkart = Flipkart()
-    while True:
-        # Extract data from the current page
-        Flipkart.loadDataOnEachPage()
+    def nextButtonClick(self):
         # Find and click the button with text "Next" if it exists
         next_button = next(
             (
@@ -112,7 +86,56 @@ if __name__ == "__main__":
 
         if next_button:
             next_button.click()
+            return True
         else:
+            return False # here are some changes
+
+    def saveDataInCSV2(data):
+        # Save the data to a CSV file
+        csv_filename = "food_data.csv"
+        with open(csv_filename,mode= "w", newline="") as csvfile:
+            csv_writer = csv.writer(csvfile)
+
+            # Write the header row
+            csv_writer.writerow(
+                ["Food Title", "Author Name", "Image URL", "Recipe URL"]
+            )
+
+            # Write the data rows
+            csv_writer.writerows(data)
+
+        print(f"Data saved to {csv_filename}")
+
+    def saveDataInCSV(self):
+        try:
+            csv_filename = "food_data.csv"
+
+            # Check if the file already exists
+            file_exists = os.path.exists(csv_filename)
+
+            with open(csv_filename, "a", newline="") as csvfile:
+                csv_writer = csv.writer(csvfile)
+
+                # Write the data rows
+                if not file_exists:
+                    # If the file is new, write the header row
+                    csv_writer.writerow(["Food Title", "Author Name", "Image URL", "Recipe URL"])
+
+                # Write the data rows
+                csv_writer.writerows(self.data)
+            print(f"Data saved to {csv_filename, len( self.data)}")
+        except Exception:
+            print("not found")
+
+if __name__ == "__main__":
+    flipkart = Flipkart()
+    while True:
+        # Extract data from the current page
+        flipkart.loadDataOnEachPage()
+        flipkart.saveDataInCSV()
+        print(driver.current_url)
+        if not flipkart.nextButtonClick():
             break
-    Flipkart.saveDataInCSV()
+            
+    # flipkart.saveDataInCSV()
     print("Task completed")
